8cf155c64cecc33837fba9773b58117f3c379ef1
Ovoshnig <boost45644@gmail.com>
2024-10-08 14:07:01 +0700

    Flying monster now has animation

--- Assets/Scripts/Monsters/FlyingMonster.cs
+++ Assets/Scripts/Monsters/FlyingMonster.cs
@@ -1,3 +1,28 @@
+using UnityEngine;
+
 public class FlyingMonster : Monster
 {
+    [SerializeField, Min(0f)] private float _rotationSpeed;
+    [SerializeField, Min(0f)] private float _maxTilt;
+
+    private int _direction;
+
+    protected override void Start()
+    {
+        base.Start();
+        _direction = Random.Range(0, 2) == 0 ? -1 : 1;
+    }
+
+    protected override void Update()
+    {
+        float rotationZ = transform.eulerAngles.z;
+
+        if (rotationZ > 180f)
+            rotationZ -= 360f;
+
+        if (Mathf.Abs(rotationZ) > _maxTilt)
+            _direction = -_direction;
+
+        transform.Rotate(0f, 0f, _direction * _rotationSpeed * Time.deltaTime);
+    }
 }
