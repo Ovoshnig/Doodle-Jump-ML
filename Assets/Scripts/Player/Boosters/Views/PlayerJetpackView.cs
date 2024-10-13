5c4dc87174a36a2d96eb0d403a6f2dc43adf2e36
Ovoshnig <boost45644@gmail.com>
2024-10-14 00:24:28 +0700

    Simplified jetpack animations logic

--- Assets/Scripts/Player/Boosters/Views/PlayerJetpackView.cs
+++ Assets/Scripts/Player/Boosters/Views/PlayerJetpackView.cs
@@ -2,13 +2,5 @@ using UnityEngine;
 
 public class PlayerJetpackView : PlayerBoosterView
 {
-    private const string SpeedMultiplierName = "speedMultiplier";
 
-    [SerializeField] private AnimationClip _runningClip;
-
-    protected override void PlayRunningAnimation(float targetDuration)
-    {
-        base.PlayRunningAnimation(targetDuration);
-        Animator.SetFloat(SpeedMultiplierName, _runningClip.length / targetDuration);
-    }
 }
